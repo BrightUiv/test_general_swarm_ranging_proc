58a2280cd73c05736561a99a436c08a2a59e9467
BrightUiv <19552605785@163.com>
2024-05-01 12:17:51 +0800

    change some code

--- hello.c
+++ hello.c
@@ -3,8 +3,8 @@
 int main()
 {
     printf("hello version 2\n");
-    printf("hello version 3\n");
-    printf("hello version 4\n");
-    printf("hello version 5\n");
+    printf("hello version 2\n");
+    printf("hello version 2\n");
+    printf("hello version 2\n");
     return 0;
 }
